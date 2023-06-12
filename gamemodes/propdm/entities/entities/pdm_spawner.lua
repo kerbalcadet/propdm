e8fcb87a9879d37188378d432428cb19804ab936
kerbalcadet <kerbalcadet@gmail.com>
2023-06-11 23:52:22 -0500

    fixed dumbass timer

--- gamemodes/propdm/entities/entities/pdm_spawner.lua
+++ gamemodes/propdm/entities/entities/pdm_spawner.lua
@@ -5,6 +5,7 @@ ENT.Spawnable = true
 ENT.PrintName = "pdm spawner"
 ENT.PropPos = Vector(0,0,-100)
 ENT.PropVelRange = Vector(1000,1000,-1000)
+ENT.SpawnDelay = 0.2
 AddCSLuaFile()
 
 function ENT:Initialize()
@@ -26,34 +27,32 @@ function ENT:Think()
 end
 
 function ENT:SpawnProp(num)
-    for i = 1, num do
-        timer.Simple(0.5, function()
-            --create prop
-            local tab = table.Random(PDM_PROPS)
-            local ent = ents.Create("prop_physics")
-            
-            ent:SetModel(tab.model)
-            ent:SetPos(self:GetPos() + self.PropPos)
-            ent:Spawn()
-            
-            local phys = ent:GetPhysicsObject()
-            phys:SetMass(tab.weight)
-            ent:SetAngles(AngleRand())
+    timer.Create(tostring(self).."spawn", self.SpawnDelay, num, function()
+        --create prop
+        local tab = table.Random(PDM_PROPS)
+        local ent = ents.Create("prop_physics")
+        
+        ent:SetModel(tab.model)
+        ent:SetPos(self:GetPos() + self.PropPos)
+        ent:Spawn()
+        
+        local phys = ent:GetPhysicsObject()
+        phys:SetMass(tab.weight)
+        ent:SetAngles(AngleRand())
 
-            local vr = VectorRand()
-            vr.z = 0;
-            local vel = self.PropVelRange*vr
-            vel.z = self.PropVelRange.z
-            phys:SetVelocity(vel)
+        local vr = VectorRand()
+        vr.z = 0;
+        local vel = self.PropVelRange*vr
+        vel.z = self.PropVelRange.z
+        phys:SetVelocity(vel)
 
-            self:EmitSound("garrysmod/balloon_pop_cute.wav", 400, 100, 0.4)
+        self:EmitSound("garrysmod/balloon_pop_cute.wav", 400, 100, 0.4)
 
-            --despawning
-            timer.Create(tostring(ent).."desp",PDM_DESPTIME,1, function()
-                ent:Dissolve(false, 1, self:GetPos())
-            end)
+        --despawning
+        timer.Create(tostring(ent).."desp",PDM_DESPTIME,1, function()
+            ent:Dissolve(false, 1, self:GetPos())
         end)
-    end
+    end)
 end
 
 function ENT:StartSpawn(num)
