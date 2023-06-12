f09e1563bf59611ec0bc51d4170951a8f9e41792
John Woodruff <john@woodruffcomp.com>
2023-06-12 13:11:28 -0500

    Attempt to move pdm_spawner on each call of SpawnProp()

---
+++ gamemodes/propdm/entities/entities/pdm_spawnzone.lua
@@ -0,0 +1,10 @@
+ENT.Type = "brush"
+ENT.Base = "base_gmodentity"
+ENT.Category = "Prop DM"
+ENT.PrintName = "PDM Spawner Bounding Box"
+ENT.Author = "homocalypse"
+ENT.Spawnable = false
+
+function ENT:Initialize()
+    continue
+end
