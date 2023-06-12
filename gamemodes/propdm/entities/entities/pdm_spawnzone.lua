2fd9d54ce7ac2866c3a0e8e6fc07ba0a3f7471f1
John Woodruff <john@woodruffcomp.com>
2023-06-12 13:54:48 -0500

    Rudimentary movement within the spawnzone achieved on each spawnprop event

--- gamemodes/propdm/entities/entities/pdm_spawnzone.lua
+++ gamemodes/propdm/entities/entities/pdm_spawnzone.lua
@@ -1,10 +1,5 @@
 ENT.Type = "brush"
-ENT.Base = "base_gmodentity"
-ENT.Category = "Prop DM"
+ENT.Base = "base_brush"
 ENT.PrintName = "PDM Spawner Bounding Box"
 ENT.Author = "homocalypse"
 ENT.Spawnable = false
-
-function ENT:Initialize()
-    continue
-end
