97dfabcdf974b600c41c8033a0b2bdd2a7d81ea1
John Woodruff <john@woodruffcomp.com>
2023-06-12 20:17:40 -0500

    Hook npc_helicopter firing to spawn props. Requires map update to function.

--- gamemodes/propdm/gamemode/game/sv_gamelogic.lua
+++ gamemodes/propdm/gamemode/game/sv_gamelogic.lua
@@ -6,3 +6,35 @@ function RoundStart()
         gamemode.Call("PlayerSpawn", p)
     end
 end
+
+hook.Add("EntityFireBullets", "Propageddon", function(v,bullet)
+	if v:IsNPC() and v:GetClass() == "npc_helicopter" and SERVER then
+			if math.random(100) < 25 then
+                local tab = table.Random(PDM_PROPS)
+                local ent = ents.Create("prop_physics")
+
+                ent:SetModel(tab.model)
+                ent:SetPos(bullet.Src + bullet.Dir * 32)
+                ent:Spawn()
+                local phys = ent:GetPhysicsObject()
+                phys:SetMass(tab.weight)
+                ent:SetAngles(AngleRand())
+                ent:SetOwner(v)
+
+                local vr = VectorRand()
+                vr.z = 0;
+                local vel = Vector(3000,3000,-3000)*vr
+                vel.z = Vector(3000,3000,-3000).z
+                phys:SetVelocity(vel)
+                
+                v:EmitSound("garrysmod/balloon_pop_cute.wav", 400, 100, 0.4)
+
+                --despawning
+                timer.Create(tostring(ent).."desp",PDM_DESPTIME,1, function()
+                    if not IsValid(ent) then return end
+                    ent:Dissolve(false, 1, self:GetPos())
+                end)
+            end
+		return false
+	end
+end)
