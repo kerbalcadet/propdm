ff50e53dd6f98f26df82e21bc5ba0b10f463c61d
kerbalcadet <kerbalcadet@gmail.com>
2023-06-12 20:50:14 -0500

    playerspawn functions

---
+++ gamemodes/propdm/gamemode/game/sv_player.lua
@@ -0,0 +1,13 @@
+function GM:PlayerInitialSpawn(ply)
+end
+
+function GM:PlayerSpawn(ply)
+    ply:StripWeapons()
+    ply:StripAmmo()
+    ply:Give("pdm_crowbar")
+    ply:Give("pdm_kirby")
+    ply:SetModel("models/player/hostage/hostage_04.mdl")
+    ply:SetupHands()
+
+    print("test")
+end
--- gamemodes/propdm/gamemode/init.lua
+++ gamemodes/propdm/gamemode/init.lua
@@ -1,8 +1,8 @@
 include("shared.lua")
---include("cl_init.lua")
 include("config/sv_globals.lua")
 include("game/sv_entities.lua")
 include("game/sv_gamelogic.lua")
+include("game/sv_player.lua")
 include("util/sv_fileutil.lua")
 
 
