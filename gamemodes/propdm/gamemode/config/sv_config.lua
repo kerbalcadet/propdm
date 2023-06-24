b19d7dc8669b5233c869a177bdcd7bae9dff34d5
kerbalcadet <kerbalcadet@gmail.com>
2023-06-24 18:19:43 -0500

    changed convar default

--- gamemodes/propdm/gamemode/config/sv_config.lua
+++ gamemodes/propdm/gamemode/config/sv_config.lua
@@ -2,5 +2,5 @@ PDM_DESPTIME = CreateConVar("pdm_despawntime", 60, 0, "Number of seconds spawned
 PDM_KILLGOAL = CreateConVar("pdm_killgoal", 30, 0, "Number of kills needed to end the round", 1)
 PDM_MAXPROPS = CreateConVar("pdm_maxprops", 100, 0, "Number of props the heli can spawn before they begin to be removed", 1)
 PDM_TIMEBEFOREROUND = CreateConVar("pdm_timebeforeround", 15, 0, "Number of seconds before a round starts", 0)
-PDM_TIMEAFTERROUND = CreateConVar("pdm_timeafterround", 5, 0, "Number of seconds after a round ends before the timer starts for the next one", 0)
+PDM_TIMEAFTERROUND = CreateConVar("pdm_timeafterround", 10, 0, "Number of seconds after a round ends before the timer starts for the next one", 0)
 PDM_SLOMO = CreateConVar("pdm_slomo", 25, 0, "Timescale (1 to 100%) for slomo when the round ends", 1, 100)
