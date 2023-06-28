b180fb3c3a3258f19d8625d09256536c0dddaad0
kerbalcadet <kerbalcadet@gmail.com>
2023-06-28 01:54:23 -0500

    added cancook to remove some logic checks

--- gamemodes/propdm/entities/weapons/pdm_carepkg_nade.lua
+++ gamemodes/propdm/entities/weapons/pdm_carepkg_nade.lua
@@ -5,12 +5,14 @@ SWEP.Base = "pdm_nade"
 SWEP.Slot = 4
 SWEP.SlotPos = 1
 
+SWEP.CanCook = false
 SWEP.Primary = {
     ClipSize = 1,
     DefaultClip = 1,
     Automatic = false, 
     Ammo = "carepkgnade"
 }
+
 
 function SWEP:ThrowNade()
     local nade = ents.Create("proj_pdm_carepkgnade")
