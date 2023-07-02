1276570158877f155914db9ace4b8f6df59b2f00
kerbalcadet <kerbalcadet@gmail.com>
2023-07-01 22:01:52 -0500

    moved grav explosion to sh_props

--- gamemodes/propdm/gamemode/game/sh_props.lua
+++ gamemodes/propdm/gamemode/game/sh_props.lua
@@ -219,6 +219,30 @@ function PDM_PropExplode(tabs, pos, vel, normal, maxang, att)
     return props
 end
 
+--helper function since multiple weapons use gravity explosions
+function PDM_GravExplode(pos, rad, pwr, minrad, plyw, att)
+    for _,ent in pairs(ents.FindInSphere(pos, rad)) do
+        local phys = ent:GetPhysicsObject()
+        if not ent:IsSolid() or not phys:IsValid() then continue end
+
+        local diff = ent:GetPos() + phys:GetMassCenter() - pos
+        local dir = diff:GetNormalized()
+        local dist = math.Clamp(diff:Length()/12, minrad, rad/12)	--feet bc why not
+        local force = (dir/(dist^(3/2)))*pwr
+        local plyweight = plyw or 2400
+
+        ent:SetPos(ent:GetPos()+Vector(0,0,1))  --remove ground friction
+
+        if(ent:IsPlayer() or ent:IsNPC()) then     --applyforce doesn't work for players
+            ent:SetVelocity(ent:GetVelocity() + force/plyw)
+        else
+            phys:ApplyForceCenter(force)
+        end
+
+        ent.Attacker = att
+    end
+end
+
 --properly attribute prop damage
 hook.Remove("EntityTakeDamage", "PDM_PropDamage")
 hook.Add("EntityTakeDamage", "PDM_PropDamage", function(ent, dmg)
